629f1521a0dc068e59fc437e30cb36aa5a940284
Denys Daneliuk <d.danelyuk@gmail.com>
2024-07-08 22:45:08 +0300

    visulizer context

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,15 @@
-import Image from "next/image";
+'use client';
+
+import {useSortingAlgorithmContext} from "./context/Visulizer";
+import {useContext, useEffect} from "react";
 
 export default function Home() {
+  const {arrayToSort, isSorting} = useSortingAlgorithmContext();
+
+  useEffect(() => {
+    console.log(arrayToSort);
+    console.log(isSorting);
+  }, [])
   return (
     <div>Test</div>
   );
