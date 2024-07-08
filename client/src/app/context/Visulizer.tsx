fcfce2a061dc56b2cc4942d572f601a471015087
Denys Daneliuk <d.danelyuk@gmail.com>
2024-07-08 23:29:44 +0300

    main layout

--- client/src/app/context/Visulizer.tsx
+++ client/src/app/context/Visulizer.tsx
@@ -22,7 +22,7 @@ interface SortingAlgorithmContextType {
 const SortingAlgorithmContext = createContext<SortingAlgorithmContextType | undefined>(undefined)
 
 export const SortingAlgorithmProvider = ({children}: {children: React.ReactNode}) => {
-  const [arrayToSort, setArrayToSort] = useState<number[]>([]);
+  const [arrayToSort, setArrayToSort] = useState<number[]>([300, 450, 120, 350, 460]);
   const [selectedAlgorithm, setSelectedAlgorithm] = useState<SortingAlgorithmType>("bubble");
   const [isSorting, setIsSorting] = useState<boolean>(false);
   const [animationSpeed, setAnimationSpeed] = useState<number>(MAX_ANIMATION_SPEED);
