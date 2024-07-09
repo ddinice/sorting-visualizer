8097b885b3ed2f67ea01bba167282d4edff18f34
Denys Daneliuk <d.danelyuk@gmail.com>
2024-07-09 23:56:29 +0300

    fix generate function

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -2,5 +2,5 @@ export const MIN_ANIMATION_SPEED = 100;
 export const MAX_ANIMATION_SPEED = 400;
 
 export function generateRandomNumberFromInterval(min: number, max: number){
-  return Math.floor(Math.random() * (max - max + 1) + min)
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
