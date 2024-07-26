6de52cb84045bbef9d395f19efb3b38f51dba301
Denys Daneliuk <d.danelyuk@gmail.com>
2024-07-26 16:50:45 +0300

    refactor bubble

--- client/src/algorithms/bubbleSort.ts
+++ client/src/algorithms/bubbleSort.ts
@@ -1,22 +1,24 @@
 import { AnimationArrayType } from "@/lib/types";
 
-function runBubbleSort(array: number[], animations: AnimationArrayType){
-  for (let i=0; i < array.length - 1; i++){
-    for (let j=0; j < array.length - 1 - 1; j++){
-      animations.push([[j, j +1], false]);
-      if (array[j] > array[j +1]) {
-        animations.push([[j, array[j + 1]], true]);
-        animations.push([[j, array[j]], true]);
-        [array[j], array[j + 1]] = [array[j + 1], array[j]];
-      }
+function runBubbleSort(array: number[], animations: AnimationArrayType) {
+  for (let i = 0; i < array.length; i++) {
+    animations.push([[i, i], false])
+    if(array[i] > array[i + 1]){
+      animations.push([[i, array[i]], true]);
+      animations.push([[i - 1, array[i - 1]], true]);
+      [array[i], array[i + 1]] = [array[i + 1], array[i]];
+    } else if (array[i] > array[i - 1]){
+      animations.push([[i, array[i]], true]);
+      animations.push([[i - 1, array[i - 1]], true]);
     }
+   
   }
 }
 
 export function generateBubbleSortAnimationArray(
   isSorting: boolean,
   array: number[],
-  runAnimation: (animations: AnimationArrayType) => void
+  runAnimation: (animation: AnimationArrayType) => void
 ) {
   if (isSorting) return;
   if (array.length <= 1) return [];
